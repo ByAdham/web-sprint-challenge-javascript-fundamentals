675c589d701646b76f7151999f70e45a7aac2dce
Adham AbdelFattah <adham.adel@gmail.com>
2020-10-16 10:53:41 -0500

    finished closure

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,19 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: They are both within the same scope which is that of function myFunction. nestedFunction was also invoked within the execution scope
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+const summation = function(a){
+  let counter = 0;
+   for (let i=1; i<=a; i++) {
+     counter = counter + i
+   }
+   return counter;
+ }
+ 
+ console.log(summation(5));
